c195d59ce8e02dc038cd9fcd08c3bf65e98b8d51
Miguel Ramos <return.infox@gmail.com>
2023-06-09 10:25:52 -0400

    aplicacion de spring con controller

--- src/main/java/cl/awakelab/bootcamp/controllers/StudentController.java
+++ src/main/java/cl/awakelab/bootcamp/controllers/StudentController.java
@@ -1,8 +1,16 @@
 package cl.awakelab.bootcamp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StudentController {
+  
+  // localhost:8080/students
+  // localhost:8080 => http://localhost:8080/
+  @GetMapping({"/students", "/"})
+  public String getAllStudents() {
+    return "students";
+  }
 
 }
