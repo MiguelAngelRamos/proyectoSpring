8ba0fd432e28f54cfd62a900f286655800514f22
Miguel Ramos <return.infox@gmail.com>
2023-06-12 10:23:10 -0400

    create user

--- src/main/java/cl/awakelab/bootcamp/controllers/StudentController.java
+++ src/main/java/cl/awakelab/bootcamp/controllers/StudentController.java
@@ -1,7 +1,10 @@
 package cl.awakelab.bootcamp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import cl.awakelab.bootcamp.entitys.Student;
 
 @Controller
 public class StudentController {
@@ -12,6 +15,14 @@ public class StudentController {
   @GetMapping({"/students", "/"})
   public String getAllStudents() {
     return "students";
+  }
+  // http://localhost:8080/students/new
+  @GetMapping("/students/new")
+  public String createStudentForm(Model model) {
+    
+    Student student = new Student();
+    model.addAttribute("student", student);
+    return "create-student";
   }
 
 }
