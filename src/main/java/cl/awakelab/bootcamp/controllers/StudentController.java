aa28df17307b80f91cbd92d4473eab0bff0fc982
Miguel Ramos <return.infox@gmail.com>
2023-06-12 10:47:29 -0400

    controlador nuevo estudiante

--- src/main/java/cl/awakelab/bootcamp/controllers/StudentController.java
+++ src/main/java/cl/awakelab/bootcamp/controllers/StudentController.java
@@ -3,6 +3,8 @@ package cl.awakelab.bootcamp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import cl.awakelab.bootcamp.entitys.Student;
 
@@ -23,6 +25,13 @@ public class StudentController {
     Student student = new Student();
     model.addAttribute("student", student);
     return "create-student";
+  }
+  
+  //http://localhost:8080/students
+  @PostMapping("/students")
+  public String saveStudent(@ModelAttribute("student") Student student) {
+    // Nosotros necesitamos llamar al Servicio y el servicio crea el recurso en la base de datos por medio de un DAO
+    return "redirect:/students";
   }
 
 }
