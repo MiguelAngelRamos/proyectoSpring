a0a87a7cd4b568c5644e61fafbebecb9cc3b6a64
Miguel Ramos <return.infox@gmail.com>
2023-06-13 10:20:11 -0400

    metodos del controlador y el servicio listos para un estudiante

--- src/main/java/cl/awakelab/bootcamp/controllers/StudentController.java
+++ src/main/java/cl/awakelab/bootcamp/controllers/StudentController.java
@@ -1,5 +1,6 @@
 package cl.awakelab.bootcamp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,18 +8,25 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import cl.awakelab.bootcamp.entitys.Student;
+import cl.awakelab.bootcamp.services.IStudentService;
 
 @Controller
 public class StudentController {
   
-  // localhost:8080/students
-  // http://localhost:8080/
-  // http://localhost:8080
+  private final IStudentService service;
+  
+  @Autowired
+  public StudentController(IStudentService service) {
+    this.service = service;
+  }
+  
+  // locahost:8080/students
   @GetMapping({"/students", "/"})
-  public String getAllStudents() {
+  public String getAllStudents(Model model) {
+    model.addAttribute("students", service.listAllStudents());
     return "students";
   }
-  // http://localhost:8080/students/new
+ 
   @GetMapping("/students/new")
   public String createStudentForm(Model model) {
     
@@ -27,11 +35,26 @@ public class StudentController {
     return "create-student"; // create-student.html
   }
   
-  //http://localhost:8080/students
   @PostMapping("/students")
   public String saveStudent(@ModelAttribute("student") Student student) {
-    // Nosotros necesitamos llamar al Servicio y el servicio crea el recurso en la base de datos por medio de un DAO
+    service.saveStudent(student);
     return "redirect:/students";
   }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
 }
